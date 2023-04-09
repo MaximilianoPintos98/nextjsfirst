c93d2fb946bc2eadabbae9edae3b53ebe723184e
MaximilianoPintos98 <pintosmaxii98@gmail.com>
2023-04-09 14:55:38 -0300

    first commit

---
+++ app/users/[id]/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+
+  return data.data;
+}
+
+async function UsersPage({ params }) {
+  const user = await getUser(params.id);
+
+  return (
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <div className="card">
+          <div className="card-header text-center">
+            <Image src={user.avatar} alt={user.avatar} />
+          </div>
+          <div className="card-body text-center">
+            <h3>
+              {user.id} {user.first_name} {user.last_name}
+            </h3>
+            <p>{user.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UsersPage;
